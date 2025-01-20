931839d3e099c4b7249031ec6fb44b02dd9f8f70
dilhararangana@gmail.com <dilhararangana@gmail.com>
2025-01-21 04:13:23 +0530

    commit changes

--- ReactAsp.NetCoreWebAPIApp/Startup.cs
+++ ReactAsp.NetCoreWebAPIApp/Startup.cs
@@ -118,7 +118,7 @@ namespace ReactAsp.NetCoreWebAPIApp
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
